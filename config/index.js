81b2014ba09412b2e4cf1b67d3b63f0cfa0316d1
Tommy Stigen Olsen <tommysolsen@gmail.com>
2019-12-27 02:03:42 +0100

    set path

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../www'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/tell',
 
     /**
      * Source Maps
