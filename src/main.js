5991ecb2d7eb1c8c7810decd7fd6d335e09b3e53
Tommy Stigen Olsen <tommysolsen@gmail.com>
2019-12-28 03:16:27 +0100

    Revert css removal

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import Framework7 from 'framework7/dist/framework7.esm.bundle.js';
 // Import F7 Vue Plugin
 import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js';
 
+// Import F7 Styles
+import Framework7Styles from 'framework7/dist/css/framework7.css';
+
+// Import Icons and App Custom Styles
+import IconsStyles from './css/icons.css';
+import AppStyles from './css/app.css';
+
 // Import Routes
 import Routes from './routes.js'
 
