d5753d2eac236d4eddb8003f1dcdda1d05f2ea61
Tommy Stigen Olsen <tommysolsen@gmail.com>
2021-12-27 15:22:27 +0100

    Update panel-left.vue

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -3,6 +3,7 @@
     <f7-navbar title="Tellemeny"></f7-navbar>
     <f7-block-title>Options</f7-block-title>
     <f7-list>
+       <f7-list-item link="/" title="Home" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/count/" title="Count" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/search/" title="Search" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/entries/" title="My entries"></f7-list-item>
